864842b650a807769095700a1c36900deb3998bc
JaslineSaro0307 <jsaroxavier1208@gmail.com>
2023-07-29 19:53:32 +0530

    menu updates

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import { Loginpage } from './components/login page/loginpage.js';
 import {BrowserRouter,Routes,Route}from 'react-router-dom'
+import { Admin } from './components/menu/admin';
+import {Client} from './components/menu/client';
+
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
     <BrowserRouter>
     <Routes>
       <Route path='/' element={[<Loginpage/>]}/>
+      <Route path='/admin' element={[<Admin/>]}/>
+      <Route path='/client' element={[<Client/>]}/>
+
     </Routes>
     </BrowserRouter>
     </>
---
+++ src/components/menu/admin.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import clg from './Microcollege.png'
+import './work.css'
+export function Admin(){
+
+    return(
+
+        <>
+
+        {/* /.......................NavBar............................./ */}
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#"><img src={clg} className="img"/></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent col-lg-6">
+    <ul class="navbar-nav  ">
+      <li class="nav-item ">
+        <Link class="nav-link" to='/'><h6 className="hv">Admin</h6></Link>
+      </li>
+      <li class="nav-item">
+      <Link class="nav-link" to='/admin'><h6 className="hv">Add Users</h6></Link>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle " href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+        <h6 className="hv">Centers</h6>
+        </a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="#">Coimbatore</a>
+          <a class="dropdown-item" href="#">Chennai</a>
+          <a class="dropdown-item" href="#">Thrissur</a>
+          <a class="dropdown-item" href="#">Bangalore</a>
+        </div>
+         
+      </li>
+      <li class="nav-item ">
+        <Link class="nav-link" to='/trainer'><h6 className="hv">Trainers</h6></Link>
+      </li>
+    </ul>
+    </div>
+</nav>
+        </>
+    );
+}
